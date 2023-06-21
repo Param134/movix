586529aa56a0fb60fbe9846c0149d96c0ff48dc9
Param <paramraval411@gmail.com>
2023-06-21 22:29:02 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { getApiConfiguration, getGenres } from './store/homeSlice';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
-import Footer from './components/footer/footer';
+import Footer from './components/footer/Footer';
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
